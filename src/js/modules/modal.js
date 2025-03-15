564acc6ce49c3db1d0e45f6de5e81ca3365ba4d0
putn1k <skachok11@yandex.ru>
2025-03-15 16:49:59 +0300

    добавил страницу услуг

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -6,7 +6,34 @@ import {
   initModal,
 } from './utils.js';
 
-const simpleModal = new HystModal( modalConfig );
+const setupServiceModal = ( modal ) => {
+  if ( modal.starter ) {
+    const card = modal.starter.closest( '.service-card' );
+    const img = card.querySelector( '.service-card__img img' );
+    const title = card.querySelector( '.service-card__title' );
+    const modalImg = modal.openedWindow.querySelector( '#service-modal-image' );
+    const modalTitle = modal.openedWindow.querySelector( '#service-modal-display' );
+    const modalInput = modal.openedWindow.querySelector( '#service-modal-input' );
+
+    modalImg.src = img ? img.src : '#';
+    modalTitle.textContent = title.textContent;
+    modalInput.value = title.textContent;
+  }
+};
+
+const beforeOpenCb = ( modal ) => {
+  switch ( modal.openedWindow.id ) {
+    case 'order-service-modal':
+      setupServiceModal( modal );
+      break;
+    default:
+      return;
+  }
+};
+
+const simpleModal = new HystModal( Object.assign( modalConfig, {
+  beforeOpen: beforeOpenCb,
+} ) );
 
 const initModals = () => {
   initModal( simpleModal );
